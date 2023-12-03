398c4aec71a859b98b9fadfbcfb0589a44fd4567
jireyop <bishalmagrati85@gmail.com>
2023-12-03 13:40:24 +0545

    farmer table
    created

---
+++ components/table/farmer_product.php
@@ -0,0 +1,38 @@
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<table class="table table-dark">
+  <thead>
+    <th>P_id</th>
+    <th>P_Name</th>
+    <th>Details</th>
+    <th>Price</th>
+    <th>Quantity</th>
+    <th>image</th>
+
+
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<div class="container">
+    <input type="button"  class="btn btn-primary m-2" value="ADD NEW">
+    <input type="button" class="btn btn-info m-2" value="EDIT">
+    <input type="button" class="btn btn-danger m-2" value="DELETE">
+
+
+
+
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
