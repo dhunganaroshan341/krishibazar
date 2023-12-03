23129dcd5eea95367de083020abb2918c0e5d0e4
jireyop <bishalmagrati85@gmail.com>
2023-12-03 13:10:27 +0545

    footer created
    components

---
+++ components/footer.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Website</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+    }
+
+    .footer {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+
+    .footer a {
+      color: #fff;
+      text-decoration: none;
+    }
+
+    .footer a:hover {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+<body>
+
+  <!-- Your website content goes here -->
+
+  <div class="footer">
+    <p>&copy; 2023 Your Company. All rights reserved. | 
+      <a href="#">Privacy Policy</a> | 
+      <a href="#">Terms of Service</a></p>
+    <p>Contact us at: <a href="tel:+123456789">+1 (234) 567-89</a> | <a href="mailto:info@yourcompany.com">info@yourcompany.com</a></p>
+    <p>Follow us on: 
+      <a href="https://www.facebook.com/yourcompany" target="_blank" rel="noopener noreferrer">Facebook</a> | 
+      <a href="https://twitter.com/yourcompany" target="_blank" rel="noopener noreferrer">Twitter</a> | 
+      <a href="https://www.linkedin.com/company/yourcompany" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+  </div>
+
+</body>
+</html>
